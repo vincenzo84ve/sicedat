9613a3433b8475ae125bfd821da8ad96bd33983e
vincenzo84ve@gmail.com <vincenzo84ve@gmail.com@3322583b-0187-aa88-4e00-730e532206be>
2011-01-28 18:58:34 +0000

    Import Inicial del Proyecto SICEDAT

---
+++ trunk/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sicedat::Application.config.secret_token = '4f1cadf5c848ebf4dc7253bbaee98fe30e12ad502dac98a8170d42d3fd2a8478db8244f4884a8b0bfd39c7a539436960e41d592dc7be8aa8669163e1b5b46cfc'
